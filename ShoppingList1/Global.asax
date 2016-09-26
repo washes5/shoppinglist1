4e9e9fcbb083e2836dd1a8b453e22dd0069f0ddb
washes5 <angiewashburn1997@gmail.com>
2016-09-26 16:29:42 -0400

    First Commit

---
+++ ShoppingList1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShoppingList1.MvcApplication" Language="C#" %>
---
+++ ShoppingList1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShoppingList1.Startup))]
+namespace ShoppingList1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
